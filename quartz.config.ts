75ad77b321149d0d396dd9a95ad627d431c8857f
Max Celant <massimiliano.celant@aa.com>
2025-06-14 14:43:16 -0500

    updating title

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
 	configuration: {
-		pageTitle: "Devlog",
+		pageTitle: "maxcelant.dev",
 		pageTitleSuffix: "",
 		enableSPA: true,
 		enablePopovers: true,
